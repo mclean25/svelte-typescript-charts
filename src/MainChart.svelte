94accf6675781d23031e1b5cf59d825049234c12
Alex McLean <alex.mclean25@gmail.com>
2021-01-09 22:43:09 -0800

    tooltip working

--- src/MainChart.svelte
+++ src/MainChart.svelte
@@ -1,31 +1,79 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import Chart from "chart.js";
-  import * as data from "../data/timeseries.json";
+  import * as salesData from "../data/timeseries.json";
+  import * as goalData from "../data/targetTimeseries.json";
 
-  console.log(data);
+  console.log("sales", salesData);
+  console.log("goal", goalData);
+
+  const roundToTwo = (num) => Math.round((num + Number.EPSILON) * 100) / 100;
+
+  function formatTooltip(tooltipItem, data) {
+    return formatNumber(tooltipItem.yLabel);
+  }
+
+  function formatAxis(label, index, labels) {
+    return formatNumber(label);
+  }
+
+  function formatNumber(num: number): string {
+    return (
+      "$" +
+      roundToTwo(num / 1000)
+        .toString()
+        .replace(/\d(?=(\d{3})+\.)/g, "$&,") +
+      "k"
+    );
+  }
 
   const renderChart = () => {
     const ctx = (<HTMLCanvasElement>(
       document.getElementById("myChart")
     )).getContext("2d");
-    var myChart = new Chart(ctx, {
+    window.chart = new Chart(ctx, {
       type: "line",
       data: {
         datasets: [
           {
-            label: "Demo",
-            data: data.default,
+            label: "Goal",
+            data: goalData.goal,
+            pointRadius: 0,
+            fill: false,
+            borderDash: [10, 5],
+          },
+          {
+            label: "My Data",
+            data: salesData.sales,
+            borderColor: "rgb(43, 51, 96)",
+            borderWidth: 2,
+            lineTension: 1,
+            pointRadius: 0,
+            pointHoverRadius: 4,
+            pointBorderWidth: 4, // point border width
           },
         ],
       },
       options: {
-        tooltips: {
-          mode: "index",
-          axis: "x",
+        hover: {
           intersect: false,
+          animationDuration: 0, // setting this back to the default (400) will break it
+        },
+        tooltips: {
+          mode: "x",
+          intersect: false,
+          callbacks: {
+            label: formatTooltip,
+          },
         },
         scales: {
+          yAxes: [
+            {
+              ticks: {
+                callback: formatAxis,
+              },
+            },
+          ],
           xAxes: [
             {
               type: "time",
